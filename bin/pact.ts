e81dcddb8d135fc78e87aa8ce81c5dd6dc4fa008
Doug Ferris <priorax@gmail.com>
2020-02-12 07:53:30 +1100

    Initial commit

---
+++ bin/pact.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { PactStack } from '../lib/pact-stack';
+
+const app = new cdk.App();
+new PactStack(app, 'PactStack');
